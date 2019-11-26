ecfe6f601e50e6930082917e8ecd8fd9fda9abcb
SleepyJen <jennifer.ni0924@gmail.com>
2019-11-25 16:36:21 -0800

    finished the band section

--- JavaScript/liri.js
+++ JavaScript/liri.js
@@ -1,20 +1,38 @@
 const ax = require('axios');
+const Spotify = require('node-spotify-api');
 
 require("dotenv").config();
 
 var keys = require('./keys.js');
 var moment = require('moment');
+var input = process.argv[2];
 
-//var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
 function concertThis(artist) {
     const urlBand = "https://rest.bandsintown.com/artists/" + artist + `/events?app_id=${process.env["BAND_ID"]}`;
     ax.get(urlBand).then(result => {
-        console.log(result.data[0]);
         const venue = result.data[0].venue;
-        console.log("Venue name: " + venue.name + "\nCountry: " + venue.country + "\nCity: " + venue.city + ", " + venue.region + "\n");
+        const time = result.data[0].datetime;
 
+        console.log("\nVenue name: " + venue.name + "\nCountry: " + venue.country + "\nCity: " + venue.city + ", " + venue.region);
+        console.log("Date: " + moment(time).format('LLLL') + "\n");
     });
 }
 
-concertThis("Celine Dion");
+function spotifyIt(song) {
+    spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data);
+    });
+}
+
+function main() {
+    if (input === 'concert-this') {
+        let name = process.argv[3];
+        concertThis(name);
+    }
+}
